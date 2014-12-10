2ca249abf6280d06b0315f8725f4e6e6ee054846
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2014-12-10 00:46:38 -0800

    Works better

--- micSSL/serial_test.py
+++ micSSL/serial_test.py
@@ -16,26 +16,28 @@ r = 0.5773502*DISTANCE_MIC     # r = l/sqrt(3)
 R = 0.8                         #    mt (Radius of Table)
 K1 = r*r + R*R
 K2 = 2*r*R
-DELAY_DIFF_THRESH = 0.1
+DELAY_DIFF_THRESH = 0.001
 ERR_THRESH =  0.05                 # test
-sign = 1
 D = 0
+SILENCE_THRESHOLD = 550
 
 def movingaverage(interval, window_size):
     window = np.ones(int(window_size))/float(window_size)
     return np.convolve(interval, window, 'valid')
 
-def f(phi):
-    y = D - np.sqrt(K1 - K2*np.cos(phi + 2*np.pi/3)) + np.sqrt(K1 - K2*np.cos(phi))
+def f1(phi):
+    y = -delayDiff12 + np.sqrt(K1 - K2*np.cos(2*np.pi/3 - phi)) - np.sqrt(K1 - K2*np.cos(phi))
     return y
 
-color1 = 0
-color2 = 0
-color3 = 0
+def f2(phi):
+    y = delayDiff23 + np.sqrt(K1 - K2*np.cos(2*np.pi/3 + phi)) - np.sqrt(K1 - K2*np.cos(phi))
+    return y
 
+def f3(phi):
+    y = -delayDiff31 + np.sqrt(K1 - K2*np.cos(2*np.pi/3 + phi)) - np.sqrt(K1 - K2*np.cos(2*np.pi/3 - phi))
+    return y
 
 ser = serial.Serial('/dev/tty.usbmodem1452', 921600, timeout=0.1)
-
 ser.close()
 ser.open()
 
@@ -67,9 +69,9 @@ while(1):
     if(i >= 250):
         startTime = time.time()
 #         print "Serial (ms):", "%.3f"%((time.time() - endTime)*1000)
-
-#         print (np.max(array1)), +(np.max(array2)), +(np.max(array3))
-        
+    
+        avMax = (np.max(array1) + (np.max(array2)) + (np.max(array3)))/3
+                
         array1 = (array1/np.linalg.norm(array1)) 
         array2 = (array2/np.linalg.norm(array2)) 
         array3 = (array3/np.linalg.norm(array3))   
@@ -85,10 +87,9 @@ while(1):
         delayDiff12 =  (np.argmax(corr12) - 125.0)*SPEED_SOUND/SAMPLE_RATE      # metres
         delayDiff23 =  (np.argmax(corr23) - 125.0)*SPEED_SOUND/SAMPLE_RATE
         delayDiff31 =  (np.argmax(corr31) - 125.0)*SPEED_SOUND/SAMPLE_RATE
-
          
         max_index = np.argmax([delayDiff12, delayDiff23, delayDiff31])
-    
+        
 #         plt.subplot(2, 1, 1)
 #         plt.plot(corr12)
 #         plt.subplot(2, 1, 2)
@@ -105,36 +106,50 @@ while(1):
         while(ser.readline().strip()!="Go"):
             print_index+=1;
             try:
-                if max_index == 0 and delayDiff12 > DELAY_DIFF_THRESH:
+                if max_index == 0 and delayDiff12 > DELAY_DIFF_THRESH and avMax > SILENCE_THRESHOLD:
                     ser.write("G")
-#                     D = delayDiff12
-#                     theta = opt.newton(f, 0, tol=1.48e-03)
-#                     ser.write(chr(int((theta*180/math.pi + 0)/20)))      
+                    theta1 = (opt.fsolve(f1, 0))
+                    theta2 = (opt.fsolve(f2, 0))
+                    theta3 = (opt.fsolve(f3, 0))
+                    theta = np.average([theta1, theta2, theta3])
+                    print theta*180/np.pi
+                    ser.write(chr(int((theta*180/np.pi + 240)/10)))      
                     if print_index%10 == 0:
                         print "Closest to G ||", "%.3f,%.3f,%.3f"%(delayDiff12, delayDiff23, delayDiff31)
 
                 
-                elif max_index == 1 and delayDiff23 > DELAY_DIFF_THRESH:
+                elif max_index == 1 and delayDiff23 > DELAY_DIFF_THRESH and avMax > SILENCE_THRESHOLD:
                     ser.write("B")
-#                     D = delayDiff23
-#                     theta = opt.newton(f, 0, tol=1.48e-03)
-#                     ser.write(chr(int((theta*180/math.pi + 120)/20)))      
+                    theta1 = (opt.fsolve(f1, 0))
+                    theta2 = (opt.fsolve(f2, 0))
+                    theta3 = (opt.fsolve(f3, 0))
+                    theta = np.average([theta1, theta2, theta3])
+                    print theta*180/np.pi
+                    if(theta < 0):
+                        theta += np.pi*2
+                    ser.write(chr(int((theta*180/np.pi + 0)/10)))      
                     if print_index%10 == 0:
                         print "Closest to B ||", "%.3f,%.3f,%.3f"%(delayDiff12, delayDiff23, delayDiff31)
 
-                elif max_index == 2 and delayDiff31 > DELAY_DIFF_THRESH:
+                elif max_index == 2 and delayDiff31 > DELAY_DIFF_THRESH and avMax > SILENCE_THRESHOLD:
                     ser.write("R")
-#                     D = delayDiff31
-#                     theta = opt.newton(f, 0, tol=1.48e-03)
-#                     ser.write(chr(int((theta*180/math.pi + 240)/20)))      
+                    theta1 = (opt.fsolve(f1, 0))
+                    theta2 = (opt.fsolve(f2, 0))
+                    theta3 = (opt.fsolve(f3, 0))
+                    theta = np.average([theta1, theta2, theta3])
+                    print theta*180/np.pi
+                    ser.write(chr(int((theta*180/np.pi + 120)/10)))      
                     if print_index%10 == 0:
                         print "Closest to R ||", "%.3f,%.3f,%.3f"%(delayDiff12, delayDiff23, delayDiff31)
+
                 ser.write('Y')
-            
-            except ValueError or RuntimeError or OverflowError:
-                if print_index%10 == 0:
-                    print "%.3f,%.3f,%.3f"%(delayDiff12, delayDiff23, delayDiff31)
+
+                
+            except ValueError or RuntimeError or OverflowError, e:
+#                 if print_index%10 == 0:
+#                     print "%.3f,%.3f,%.3f"%(delayDiff12, delayDiff23, delayDiff31)
 #                     print theta
+                print e
                 ser.write('Y')
 
         ser.flushInput()  
